3c6f13a3608c793c0d19f321cef3b2fd43fa63ff
1122Louis <louisdzidzor22@gmail.com>
2023-02-16 18:19:27 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - A programming with put function
+ * Return: (0)Success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+return (0);
+
+}
+
