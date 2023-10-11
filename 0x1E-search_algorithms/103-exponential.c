1fae49a4185f057964c0ec023f22984a3479f4c0
1122Louis <louisdzidzor22@gmail.com>
2023-10-11 11:57:15 -0700

    initial commit

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,73 +1,103 @@
 #include "search_algos.h"
+#include <stdio.h>
 
-int _binary_search(int *array, size_t left, size_t right, int value);
 
 /**
-* exponential_search - Searches for a value in a sorted array
-*                      of integers using exponential search.
-* @array: A pointer to the first element of the array to search.
-* @size: The number of elements in the array.
-* @value: The value to search for.
-*
-* Return: If the value is not present or the array is NULL, -1.
-*         Otherwise, the index where the value is located.
-*
-* Description: Prints a value every time it is compared in the array.
-*/
+ * print_array - print an array from a start to end index
+ * @array: array to print
+ * @start: index to start at
+ * @end: index to end at
+ * Return: void
+ */
+void print_array(int *array, size_t start, size_t end)
+{
+	if (!array)
+		return;
+
+	while (start < end)
+		printf("%i, ", array[start++]);
+
+	printf("%i\n", array[start]);
+}
+
+/**
+ * binary_search - find a value in an array using binary search
+ * @array: array of integer values
+ * @size: size of the array
+ * @value: integer value to find
+ * Return: index of the value found
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t low, mid, high;
+
+	if (!array)
+		return (-1);
+
+	low = 0;
+	high = size - 1;
+	mid = low + (high - low) / 2;
+
+	while (low <= high)
+	{
+		printf("Searching in array: ");
+		print_array(array, low, high);
+
+		if (low == high)
+		{
+			if (array[low] == value)
+				return (low);
+			else
+				return (-1);
+		}
+
+		if (array[mid] < value)
+			low = mid + 1;
+		else if (array[mid] > value)
+			high = mid - 1;
+		else
+			return (mid);
+
+		mid = low + (high - low) / 2;
+	}
+
+	return (-1);
+}
+
+/**
+ * exponential_search - find a value in an array using exponential search
+ * @array: array of integer values
+ * @size: size of the array
+ * @value: integer value to find
+ * Return: index of the value found
+ */
 int exponential_search(int *array, size_t size, int value)
 {
-size_t i = 0, right;
+	size_t bound, len;
+	int index;
 
-if (array == NULL)
-return (-1);
+	if (!array)
+		return (-1);
 
-if (array[0] != value)
-{
-for (i = 1; i < size && array[i] <= value; i = i * 2)
-printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-}
+	bound = 1;
 
-right = i < size ? i : size - 1;
-printf("Value found between indexes [%ld] and [%ld]\n", i / 2, right);
-return (_binary_search(array, i / 2, right, value));
-}
+	while (bound < size && array[bound] < value)
+	{
+		printf("Value checked array[%lu] = [%i]\n", bound, array[bound]);
+		bound *= 2;
+	}
 
+	len = bound - (bound / 2);
+	printf("Value found between indexes [%lu] and ", bound / 2);
+	if (bound >= size)
+		printf("[%lu]\n", size - 1);
+	else
+		printf("[%lu]\n", bound);
 
-/**
-* _binary_search - Searches for a value in a sorted array
-*                  of integers using binary search.
-* @array: A pointer to the first element of the array to search.
-* @left: The starting index of the [sub]array to search.
-* @right: The ending index of the [sub]array to search.
-* @value: The value to search for.
-*
-* Return: If the value is not present or the array is NULL, -1.
-*         Otherwise, the index where the value is located.
-*
-* Description: Prints the [sub]array being searched after each change.
-*/
-int _binary_search(int *array, size_t left, size_t right, int value)
-{
-size_t i;
+	index = binary_search(array + bound / 2, len, value);
 
-if (array == NULL)
-return (-1);
+	if (index != -1)
+		return (index + (bound / 2));
 
-while (right >= left)
-{
-printf("Searching in array: ");
-for (i = left; i < right; i++)
-printf("%d, ", array[i]);
-printf("%d\n", array[i]);
-
-i = left + (right - left) / 2;
-if (array[i] == value)
-return (i);
-if (array[i] > value)
-right = i - 1;
-else
-left = i + 1;
-}
-
-return (-1);
+	return (index);
 }
